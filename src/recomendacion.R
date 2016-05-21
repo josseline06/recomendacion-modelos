a05b8dd84c3a6e81734944688ed974d7219b00ef
Josseline Perdomo <joss.desi@gmail.com>
2016-05-21 14:10:34 -0400

    done

--- src/recomendacion.R
+++ src/recomendacion.R
@@ -1,7 +1,7 @@
 #	1. Requerimientos de plataforma
 # -----------------------------------------------------
 source("src/utilities.R")
-utilities(c("arules", "arulesViz"))
+include(c("arules", "arulesViz"))
 
 #	2. Carga de datos
 # -----------------------------------------------------
@@ -11,12 +11,14 @@ dataset <- read.csv2("dat/periodico.csv", header = T,
 					 nrows = 131300,
 					 comment.char = "",
 					 stringsAsFactors = F)
+					 
 #	Temas de los artículos del periódico
 subjects <- c("deportes", "politica", 
 		      "variedades", "internacional", 
 		      "nacionales", "sucesos", 
 		      "comunidad", "negocios", 
 		      "opinion")
+			  
 colnames(dataset)
 
 #	3. Preprocesamiento y preparación de los datos
@@ -78,12 +80,67 @@ transactions <- transactions[bots == F]
 names(transactions) <- dataset$tid
 transactions <- as(transactions, "transactions")
 
-#	- Buscando tipos de usuarios:
+# - Tipos de Usuario
+u_transactions <- unique(transactions)
+small_sample <- sample(u_transactions, 10000)
+
+# Calculando similaridad entre itemsets
+d_jaccard <- dissimilarity(small_sample)
+d_jaccard[is.na(d_jaccard)] <- 1 # Eliminando NA
+
+# Calculando clusters
+hclusters <- hclust(d_jaccard, method = "ward")
+# Dendrograma
+pdf(file = "similarity.pdf", width = 150)
+plot(hclusters, cex = 0.5)
+dev.off()
+
+# Haciendo corte
+cutting <- cutree(hclusters, k = 8)
+
+# Descartando transacciones pertenecientes a small_sample
+big_sample <- u_transactions[!(u_transactions %in% small_sample)]
+# Prediciendo
+pred_labels <- predict(small_sample, big_sample, cutting) 
+
+# Resultados
+print(table(cutting))
+print(table(pred_labels))
+
+#	- Generando reglas:
+rules <- apriori(transactions, 
+                 parameter = list(sup = 0.00003, 
+                                  conf = 0.65, 
+                                  target="rules"))
+# Ordenando reglas por confianza
+rules <- sort(rules, by = "confidence", decreasing = T)
+
+# Viendo reglas
+plot(rules, method ="grouped", control = list(k = 50))
+
+# - Recomendador
+recommend <- function(itemset, rules){
+  suitableRules <- subset(rules,lhs %ain% itemset & !rhs %in% itemset)
+  # Si no se encuentra transacción, entonces buscar un subconjunto
+  if(length(suitableRules) == 0)
+    suitableRules <- subset(rules, lhs %in% itemset & !rhs %in% itemset)
+  
+  return(inspect(suitableRules@rhs[1]))
+}
+
+# Probando
+example <- c("deportes/articulo1",     
+             "deportes/articulo2",     
+             "deportes/articulo3")
+
+recommend_out <- recommend(example,rules)
+
 
 #	10 visitas con mayor y menor tiempo 
 by_time <- order(dataset$time, decreasing = T) 
 head(dataset[by_time,], n = 10)
 tail(dataset[by_time,], n = 10)
 
-#	10 transacciones con mayor cantidad de apariciones
-sort(dataset$articles, decreasing = T)[1:10]
+#	10 transacciones más frecuentes
+freq_itemsets <- apriori(transactions, parameter = list(sup = 0.00003, target = "frequent"))
+inspect(head(sort(freq_itemsets, decreasing = T), n = 10))
